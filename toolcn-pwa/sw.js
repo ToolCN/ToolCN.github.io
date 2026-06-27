e446cc18f216cd50e8b80c2646cd2bb2a70576b8
ToolCN <toolcn.qro@gmail.com>
2026-06-27 13:29:14 -0600

    Update sw.js

--- toolcn-pwa/sw.js
+++ toolcn-pwa/sw.js
@@ -1,15 +1,25 @@
-const CACHE = 'materia-prima-v1';
-const ARCHIVOS = ['./index.html', './manifest.json'];
+const CACHE = 'mp-v2';
+const ARCHIVOS = [
+  '/toolcn-pwa/index.html',
+  '/toolcn-pwa/manifest.json',
+  '/toolcn-pwa/icon-192.png',
+  '/toolcn-pwa/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
